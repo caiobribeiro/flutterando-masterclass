02330d3e3276f3f1d9fb12d426020b43074d5fff
Caio Beraldi Ribeiro <caio_4719@hotmail.com>
2023-02-26 09:41:23 -0300

    It was created a dart project for IMC challenge. The human class contains 3 methods, imc that returns imc valeus, imcRating that returns the rating imc based in imc value and humanImcInfo that returns concateneted String of all information

---
+++ imc_calculator/bin/imc_calculator.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+
+void main() {
+  final caio = Human(name: "Caio", height: 1.63, weight: 1116);
+  print(caio.humanImcInfo());
+}
+
+class Human {
+  late String name;
+  late double height;
+  late double weight;
+
+  Human({
+    required String name,
+    required double height,
+    required double weight,
+  }) {
+    this.name = name;
+    this.height = height;
+    this.weight = weight;
+  }
+
+  double imc() {
+    final double imcValue = weight / pow(height, 2);
+    return imcValue;
+  }
+
+  String imcRating(double imcValue) {
+    if (imcValue < 18.5) {
+      return 'Underweight';
+    } else if (imcValue > 18.5 && imcValue < 24.9) {
+      return 'Normal';
+    } else if (imcValue > 25 && imcValue < 29.9) {
+      return 'Overweight';
+    } else if (imcValue > 30 && imcValue < 35) {
+      return 'Obese';
+    } else if (imcValue > 35) {
+      return 'Extremey Obese';
+    } else {
+      return 'Some value inserted was wrong';
+    }
+  }
+
+  String humanImcInfo() {
+    final imcValue = imc();
+    final imcRatingValue = imcRating(imcValue);
+    return "IMC of $name is $imcRatingValue with imc value of $imcValue";
+  }
+}
