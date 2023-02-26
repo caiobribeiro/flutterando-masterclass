37b4ae5eb9bc81b466821de6491fd51e35225911
Caio Beraldi Ribeiro <caio_4719@hotmail.com>
2023-02-26 10:18:14 -0300

    Function that returns fibonacci sequence number

---
+++ recursive_fibonacci_calculator/test/recursive_fibonacci_calculator_test.dart
@@ -0,0 +1,8 @@
+// import 'package:recursive_fibonacci_calculator/recursive_fibonacci_calculator.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
