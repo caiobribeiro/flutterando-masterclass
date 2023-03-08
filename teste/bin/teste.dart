28f18892058b3f467a0e66819a7dbe1cd3932d51
Caio Beraldi Ribeiro <caio_4719@hotmail.com>
2023-03-08 09:26:53 -0300

    CPF validator

---
+++ teste/bin/teste.dart
@@ -0,0 +1,11 @@
+// void main(List<String> arguments) {
+//   final name = "Caio";
+
+//   String fullName(String name, String lastName) {
+//     return "$name $lastName";
+//   }
+
+//   print(name[2]);
+//   print(name.runes);
+//   print(name.runtimeType);
+// }
